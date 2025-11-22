f1bf93d17ae461af2d0c13518a45a46145554952
Misha Jitendrakumar Patel <patel.misha@northeastern.edu>
2025-11-22 18:11:27 -0500

    worked on error

--- src/com/intramural/scheduling/view/TestAnalytics.java
+++ src/com/intramural/scheduling/view/TestAnalytics.java
@@ -1,6 +1,5 @@
-package com.intramural.scheduling;
+package com.intramural.scheduling.view;
 
-import com.intramural.scheduling.view.AnalyticsPage;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
